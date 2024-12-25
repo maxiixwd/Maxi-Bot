3cbeb5b0de1651e5bfd45de9e036c0642aba0610
Mohammed Abir <153712202+Abir-rs@users.noreply.github.com>
2024-12-25 14:57:14 +0600

    Update baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -165,5 +165,5 @@ module.exports.onChat = async ({ api, event,message }) => {
     }, event.messageID);
     }
   }catch(err){
-      return api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
+      return api.sendMessage(`Error baby 🥺🥺`, event.threadID, event.messageID);
     }};
