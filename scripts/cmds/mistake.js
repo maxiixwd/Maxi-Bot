48421515befb4fd0d715ddb4e2e92422dafddbc7
Mohammed Abir <153712202+Abir-rs@users.noreply.github.com>
2024-12-30 11:42:34 +0600

    Update mistake.js

--- scripts/cmds/mistake.js
+++ scripts/cmds/mistake.js
@@ -18,35 +18,40 @@ module.exports = {
 
   onStart: async function ({ message, event, args }) {
     const mention = Object.keys(event.mentions);
-    if (mention.length == 0) {
-      message.reply("mention someone");
+
+    if (mention.length === 0) {
+      message.reply("Please mention someone.");
       return;
     }
 
-    let one;
-    if (mention.length == 1) {
-      one = mention[0];
-    } else {
-      one = mention[0];
+    let one = mention[0];
+
+    // Restrict for the specified UID
+    if (one === "100078140834638") {
+      message.reply("You are the mistake bro 🐸");
+      return;
     }
 
     try {
       const imagePath = await bal(one);
       await message.reply({
-        body: " The Biggest Mistake in Earth ",
-        attachment: fs.createReadStream(imagePath)
+        body: "The Biggest Mistake on Earth",
+        attachment: fs.createReadStream(imagePath),
       });
     } catch (error) {
       console.error("Error while running command:", error);
-      await message.reply("an error occurred");
+      await message.reply("An error occurred while processing your request.");
     }
-  }
+  },
 };
+
 async function bal(one) {
-  const avatarone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+  const avatarone = await jimp.read(
+    `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`
+  );
   const image = await jimp.read("https://i.postimg.cc/2ST7x1Dw/received-6010166635719509.jpg");
   image.resize(512, 512).composite(avatarone.resize(220, 203), 145, 305);
   const imagePath = "mistake.png";
   await image.writeAsync(imagePath);
   return imagePath;
-  }
+}
