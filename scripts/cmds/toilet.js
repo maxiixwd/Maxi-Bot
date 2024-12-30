79ddb2ae9a345a0d6785780d23432570c1afd853
Mohammed Abir <153712202+Abir-rs@users.noreply.github.com>
2024-12-30 13:28:28 +0600

    Update toilet.js

--- scripts/cmds/toilet.js
+++ scripts/cmds/toilet.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const jimp = require("jimp");
 const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   config: {
@@ -60,15 +61,24 @@ module.exports = {
 async function bal(one, two) {
   const avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
   avone.circle();
+  
   const avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
   avtwo.circle();
-  const pth = "toilet.png";
+
+  // Set path to save the image in the tmp folder
+  const tmpDir = path.join(__dirname, "tmp"); // Assuming "tmp" folder is in the same directory
+  if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir); // Create the tmp folder if it doesn't exist
+  }
+
+  const imagePath = path.join(tmpDir, "toilet.png");
+
   const img = await jimp.read("https://i.imgur.com/sZW2vlz.png");
 
   img.resize(1080, 1350)
      .composite(avone.resize(360, 360), 8828282, 2828)
      .composite(avtwo.resize(450, 450), 300, 660);
 
-  await img.writeAsync(pth);
-  return pth;
+  await img.writeAsync(imagePath);
+  return imagePath;
 }
